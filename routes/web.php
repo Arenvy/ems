1eadb6c12bc27accc426d81a2a5e35f85a343467
Arenvy <aarenvy293@gmail.com>
2025-10-01 00:49:17 +0800

    Enhanced user registration and login functionality with improved validation and success/error messaging

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,12 @@ Route::get('/', function () {
     return view('index');
 });
 
-
+//Users related function
 Route::get('/login', [UserController::class, 'loginpage'])->name('user.loginpage');
 Route::post('/login', [UserController::class, 'login'])->name('user.login');
 Route::get('/register', [UserController::class, 'registerpage'])->name('user.registerpage');
 Route::post('/register', [UserController::class, 'register']);
 
+//Dashbboard related function
 Route::post('/index', [UserController::class, 'logout'])->name('user.logout');
-Route::get('/index', [UserController::class, 'indexpage']);
+Route::get('/index', [UserController::class, 'indexpage'])->name('indexpage');
